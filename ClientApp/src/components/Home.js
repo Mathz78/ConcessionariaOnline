2e85217ccda91535d244c38a1e7074ef5547b8ca
Matheus <matheusrocha7802@gmail.com>
2021-11-15 16:53:40 -0300

    Remove some whitelines

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -5,33 +5,34 @@ import skyline from './../images/skyline.jpg';
 
 export class Home extends Component {
     static displayName = Home.name;
+    
     render () {
-    return (
-    <div>
-        <h1>Sejam bem-vindos!</h1>
+        return (
+        <div>
+            <h1>Sejam bem-vindos!</h1>
 
-        <div id="carouselExampleControls" class="carousel slide mt-5" data-ride="carousel">
-            <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100 rounded" src={skyline} alt="First slide"></img>
+            <div id="carouselExampleControls" class="carousel slide mt-5" data-ride="carousel">
+                <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100 rounded" src={skyline} alt="First slide"></img>
+                        </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 rounded" src={mclaren} alt="Second slide"></img>
                     </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100 rounded" src={mclaren} alt="Second slide"></img>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 rounded" src={corvete} alt="Third slide"></img>
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100 rounded" src={corvete} alt="Third slide"></img>
-                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
-    </div>
-    );
+        );
   }
 }
