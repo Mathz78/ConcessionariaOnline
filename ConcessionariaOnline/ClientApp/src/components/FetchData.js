a0fd4a3bb4466cd0b6d2aa7da6296d2b488fbef5
Matheus <matheusrocha7802@gmail.com>
2021-12-14 19:44:57 -0300

    Add a text about us

--- ConcessionariaOnline/ClientApp/src/components/FetchData.js
+++ ConcessionariaOnline/ClientApp/src/components/FetchData.js
@@ -17,19 +17,17 @@ export class FetchData extends Component {
       <table className='table table-striped' aria-labelledby="tabelLabel">
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
+            <th>Primeiro Nome</th>
+            <th>Último nome</th>
+            <th>Nota</th>
           </tr>
         </thead>
         <tbody>
           {forecasts.map(forecast =>
             <tr key={forecast.date}>
               <td>{forecast.name}</td>
-              <td>{forecast.username}</td>
               <td>{forecast.lastName}</td>
-              <td>{forecast.password}</td>
+              <td>5</td>
             </tr>
           )}
         </tbody>
@@ -43,10 +41,29 @@ export class FetchData extends Component {
       : FetchData.renderForecastsTable(this.state.forecasts);
 
     return (
+
       <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
+        <div>
+          <h1 id="tabelLabel" >Lista contendo os nossos principais funcionários</h1>
+          <p>Este lista contempla os melhores funcionários do mês atual!.</p>
+          {contents}
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <div class="form-row">
+                  <h1 class="text-center mt-3"> Um pouco de nossa história</h1>
+                  <p>
+                      Nós fizemos um sistema onde o cliente pode entrar no site e efetuar a compra do seu
+                      carro sem burocracia podendo fazer a compra por transferência,cartão de crédito
+                      ou débito e pix. Podendo agendar a retirada ou receber o carro em sua casa,
+                      como o sistema é online diminuiremos o número de funcionários e gastos,assim
+                      diminuindo os valores que serão repassados nas vendas dos carros,com isso
+                      nossa concessionária teria um ótimo preço no mercado.
+                  </p>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
